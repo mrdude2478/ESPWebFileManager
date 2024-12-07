a1c7a878167bf4a9522a9b7a7ae3b4f3c7927f7e
Jobit Joseph <51044706+jobitjoseph@users.noreply.github.com>
2024-12-07 20:19:04 +0530

    Update ESPWebFileManager.cpp

--- src/ESPWebFileManager.cpp
+++ src/ESPWebFileManager.cpp
@@ -1,6 +1,6 @@
 /* 
  * EspWebFileManager Library
- * Copyright (C) 2024 Your Name
+ * Copyright (C) 2024 Jobit Joseph
  * Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
  * You may not use this work for commercial purposes. Modifications must credit the original author.
  * See the LICENSE file for more details.
